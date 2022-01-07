cbd1f9932cff4e600b86983965bea5e80ea4aa6e
jktham-sf <jonas.tham@gmx.ch>
2022-01-07 12:56:36 +0100

    Sort and save duplicate city names as json

---
+++ sort.py
@@ -0,0 +1,28 @@
+import csv
+import time
+import json
+
+start_time = time.time()
+
+data = []
+cities = []
+result = []
+
+with open(r".\data\worldcities.csv", encoding="utf8") as file:
+    reader = csv.DictReader(file, delimiter=",")
+    for row in reader:
+        data.append(row)
+
+for i in range(len(data)):        
+    cities.append(data[i]["city_ascii"])
+
+for i in range(len(cities)):
+    if cities.count(cities[i]) > 1:
+        result.append(data[i])
+
+result.sort(key=lambda city: (city["city_ascii"], city["population"] * -1))
+
+with open(r".\data\result.json", "w") as file:
+    json.dump(result, file)
+
+print(time.time() - start_time)
