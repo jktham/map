f09ca7ead10474e3fc73239ca86715b2c9c32a6d
jktham-pc <jonas.tham@gmx.ch>
2022-01-08 19:35:35 +0100

    Get geopandas working with folium

--- display.py
+++ display.py
@@ -1,8 +1,11 @@
 import json
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import geopandas as gpd
+import folium as f
 
 with open(r".\data\result.json") as file:
     data = json.load(file)
 
-print(data)
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
+m = world.explore()
+m.save('map.html')
