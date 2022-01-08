29156bf601edd79954e3fc0445bfad46475c09f6
jktham-pc <jonas.tham@gmx.ch>
2022-01-08 20:11:50 +0100

    Add background map

--- display.py
+++ display.py
@@ -1,11 +1,22 @@
 import json
 import matplotlib.pyplot as plt
 import geopandas as gpd
+import pandas as pd
 import folium as f
 
 with open(r".\data\result.json") as file:
     data = json.load(file)
 
-world = gpd.read_file(gpd.datasets.get_path('naturalearth_cities'))
-m = world.explore()
-m.save('map.html')
+df = pd.DataFrame({
+    "Name": [city["city_ascii"] for city in data],
+    "Country": [city["country"] for city in data],
+    "Population": [city["population"] for city in data],
+    "Latitude": [city["lat"] for city in data],
+    "Longitude": [city["lng"] for city in data]
+})
+
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
+
+bg = f.Map()
+map = gdf.explore(m=bg, marker_kwds=dict(radius=2, fill=True))
+map.save(r'.\data\map.html')
